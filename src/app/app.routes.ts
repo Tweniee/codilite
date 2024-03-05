965e9f61af3a15b241b584f83db87ec8b829e998
Tweniee <abhisheku361@gmail.com>
2024-03-05 15:05:55 +0530

    codilite init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { OtpComponent } from './otp/otp.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CodeBoxComponent } from './code-box/code-box.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'verify-otp', component: OtpComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'code-box', component: CodeBoxComponent },
+];
