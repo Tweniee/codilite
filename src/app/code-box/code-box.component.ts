ccfb1f9aac25a6b14ae657e7c036528305a67a10
Tweniee <abhisheku361@gmail.com>
2024-04-18 15:49:13 +0530

    editor added

--- src/app/code-box/code-box.component.ts
+++ src/app/code-box/code-box.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CodeModel } from '../interface/code-model';
+import { CommonModule } from '@angular/common';
 
+import { CodeEditorModule } from '@ngstack/code-editor';
 @Component({
   selector: 'app-code-box',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CodeEditorModule],
   templateUrl: './code-box.component.html',
-  styleUrl: './code-box.component.css'
+  styleUrl: './code-box.component.css',
 })
 export class CodeBoxComponent {
+  theme = 'vs-dark';
 
+  codeModel: CodeModel = {
+    language: 'javascript',
+    uri: 'main.js',
+    value: '',
+    // dependencies: ['@types/node', '@ngstack/translate', '@ngstack/code-editor'],
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true,
+    },
+  };
+
+  onCodeChanged(value: string) {
+    console.log('CODE', value);
+  }
 }
